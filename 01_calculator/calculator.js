51d0614c33ccf32d73ca492b33f7a693992e6bc9
Yubin Park <itqkrdbqls1001@naver.com>
2021-08-16 01:20:09 +0900

    feat: function-button에 기능별 이벤트 핸들링 로직 추가

--- 01_calculator/calculator.js
+++ 01_calculator/calculator.js
@@ -1,20 +1,38 @@
+// 미지원 버튼 목록
+const unsupportedButtons = ['계산 기록', '←', 'C', '( )', '%', '÷', '×', '-', '+', '+/-', '.', '=']
+
 // 입력칸
 let inputStatus = document.getElementById('input-status');
 // 입력 창이 비어 있음을 기록하는 변수
 let isEmpty = true;
 
 // 입력칸 갱신 함수
-let updateInputStatus = function(c) {
-    inputStatus.textContent = isEmpty ? c : inputStatus.textContent + c;
-    isEmpty = false;
+let updateInputStatus = function (e) {
+    console.log(e.target.textContent.trim());
+    // function-button에 해당하는 경우
+    if (e.target.classList.contains('function-button')) {
+        let character = e.target.textContent.trim();
+
+        // 지원하지 않는 기능인 경우
+        if (unsupportedButtons.includes(character)) {
+            alert('지원하지 않는 기능입니다.');
+        } else {
+            // TODO: 버튼별 연산 기능 추가
+        }
+    }
+    // 연산자가 아닌 경우 (숫자)
+    else {
+        let character = e.target.textContent.trim();
+        inputStatus.textContent = isEmpty ? character : inputStatus.textContent + character;
+        isEmpty = false;
+    }
 }
 
 // 눌렀을 때 화면에 반영되는 버튼 목록
 let normalButtons = document.getElementsByClassName('normal-button');
 
 for (let button of normalButtons) {
-    button.addEventListener("click", function(e) {
-        let character = e.target.textContent.trim();
-        updateInputStatus(character);
+    button.addEventListener("click", function (e) {
+        updateInputStatus(e);
     });
 }
